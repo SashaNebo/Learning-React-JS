e4a1b3b9f5db85bbfecb999e64d9edb32007c92b
Alexander Nebov <118817047+SashaNebo@users.noreply.github.com>
2023-12-12 08:56:37 +0300

    init: start learn react hooks

---
+++ Ulbi TV. React JS фундаментальный курс от А до Я/react-hooks/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Counter from './components/Counter'
+import ClassCounter from './components/ClassCounter'
+
+function App() {
+  return (
+    <div className='App'>
+      <Counter />
+      <ClassCounter />
+    </div>
+  )
+}
+
+export default App
