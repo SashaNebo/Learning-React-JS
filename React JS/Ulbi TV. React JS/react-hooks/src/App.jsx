6802549aeafac7ebeee6aaeee6ca45c154668c92
Alexander Nebov <118817047+SashaNebo@users.noreply.github.com>
2024-01-03 09:28:23 +0300

    feat: continue learn react + ts

---
+++ React JS/Ulbi TV. React JS/react-hooks/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Input from './components/Input'
+import Hover from './components/Hover'
+import List from './components/List'
+import Search from './components/Search'
+import Request from './components/Request'
+
+const App = () => {
+  return <Search />
+}
+
+export default App
